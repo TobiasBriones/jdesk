672be5588d1715424880aae5296befe53ee6371f
Tobias Briones <tobiasbriones.dev@gmail.com>
2021-05-17 23:38:35 -0600

    Add package-info.java

---
+++ src/io/github/tobiasbriones/jdesk/io/package-info.java
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2018-2021 Tobias Briones. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ * This file is part of JDesk.
+ *
+ * This source code is licensed under the BSD-3-Clause License found in the
+ * LICENSE file in the root directory of this source tree or at
+ * https://opensource.org/licenses/BSD-3-Clause.
+ */
+
+/**
+ * Contains IO and File utilities and extensions.
+ */
+
+package io.github.tobiasbriones.jdesk.io;
