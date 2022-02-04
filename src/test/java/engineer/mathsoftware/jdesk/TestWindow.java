12d102d8b8134faa732a8367bf237aa19b303f0e
Tobias Briones <tobiasbriones.dev@gmail.com>
2022-02-04 17:14:35 -0600

    Add AppWork basic test

--- src/test/java/engineer/mathsoftware/jdesk/TestWindow.java
+++ src/test/java/engineer/mathsoftware/jdesk/TestWindow.java
@@ -12,23 +12,70 @@
 
 package engineer.mathsoftware.jdesk;
 
+import engineer.mathsoftware.jdesk.ui.dialog.AppDialog;
 import engineer.mathsoftware.jdesk.ui.view.Panel;
+import engineer.mathsoftware.jdesk.ui.view.loading.BarLoadingView;
+import engineer.mathsoftware.jdesk.work.AppWorkCallback;
+import engineer.mathsoftware.jdesk.work.AppWorker;
+import engineer.mathsoftware.jdesk.work.WorkCallback;
+import engineer.mathsoftware.jdesk.work.WorkRunnable;
 
 import java.awt.*;
 
 public class TestWindow extends Window {
+    private final BarLoadingView loadingView;
+
     public TestWindow(AppInstance appInstance) {
         super(appInstance);
+        this.loadingView = new BarLoadingView(this);
     }
 
     @Override
     protected void createWindow(Panel panel) {
         final String okStr = getStringResources().get(Strings.OK);
         panel.add(new Label(okStr));
+        panel.add(loadingView);
     }
 
     @Override
     protected void windowCreated() {
         setVisible(true);
+        simulateBackgroundWork();
+    }
+
+    private void simulateBackgroundWork() {
+        final WorkRunnable<String> runnable = () -> {
+            Thread.sleep(1500L);
+            return "Work completed";
+
+            // Try failing
+            // throw new RuntimeException("failed");
+        };
+        final AppWorkCallback<String> callback = new AppWorkCallback<String>(this) {
+            @Override
+            public void workFinished(String result) {
+                AppDialog.showMessage(
+                    TestWindow.this,
+                    "Background Work",
+                    result,
+                    AppDialog.Type.SUCCESS
+                );
+            }
+
+            @Override
+            public void workFailed(Exception exception) {
+                // AppWorkCallback automatically manages fails on the GUI app
+                super.workFailed(exception);
+
+                // Add further error handling
+                System.out.println("Work failed!");
+                System.out.println(exception.getMessage());
+            }
+        };
+        final AppWorker<String, Void> task = new AppWorker<>(
+            loadingView,
+            callback
+        );
+        task.execute(runnable);
     }
 }
