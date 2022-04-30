2ceeaead73a8e9bfc516cac381541680f8b7f676
Tobias Briones <tobiasbriones.dev@gmail.com>
2022-04-30 17:01:13 -0600

    Add StringResource
    This will allow to make the new strings implementation backwards compatible.

---
+++ src/main/java/engineer/mathsoftware/jdesk/resources/StringResourceId.java
@@ -0,0 +1,23 @@
+// Copyright (c) 2022 Tobias Briones. All rights reserved.
+// SPDX-License-Identifier: BSD-3-Clause
+// This file is part of https://github.com/tobiasbriones/jdesk
+
+package engineer.mathsoftware.jdesk.resources;
+
+public final class StringResourceId {
+    private final int id;
+    private final String key;
+
+    public StringResourceId(int id, String key) {
+        this.id = id;
+        this.key = key;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getKey() {
+        return key;
+    }
+}
